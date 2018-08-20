7c298f9555a359de847f66d838f793f5ebfc293d
Todd Robbins <toddrobb11@gmail.com>
2018-08-20 00:16:09 -0700

    Assignment 5 Ragdoll unity

---
+++ Visualizing wearable sensor movement using Ragdoll in Unity/Assets/Robot Kyle/scripts/leftThighMotion.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+public class leftThighMotion : MonoBehaviour {
+
+    SerialPort serial = new SerialPort("COM4", 115200);
+    
+    ArrayList gyroListX = new ArrayList();
+    ArrayList gyroListY = new ArrayList();
+    ArrayList gyroListZ = new ArrayList();
+    // Use this for initialization
+    void Start()
+    {
+        serial.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!serial.IsOpen)
+        {
+            serial.Open();
+        }
+        string[] accelerometer = serial.ReadLine().Split(',');
+        string[] gyroscope = serial.ReadLine().Split(',');
+        string[] mag = serial.ReadLine().Split(',');
+
+        transform.Translate(float.Parse(accelerometer[0]) / 10000, //x
+                            float.Parse(accelerometer[1]) / 10000, //y
+                            float.Parse(accelerometer[2]) / 10000); //z
+        float magX = float.Parse(mag[0]);
+        float magY = float.Parse(mag[1]);
+        float magZ = float.Parse(mag[2]);
+
+        float gyroscopeX = float.Parse(gyroscope[0]);
+        gyroListX.Add(gyroscopeX);
+        if (gyroListX.Count > 5)
+        {
+            gyroListX.RemoveAt(0); 
+        }
+
+        float gyroscopeY = float.Parse(gyroscope[1]);
+        gyroListY.Add(gyroscopeY);
+        if (gyroListY.Count > 5) {
+            gyroListY.RemoveAt(0);
+        }
+        float gyroscopeZ = float.Parse(gyroscope[2]);
+        gyroListY.Add(gyroscopeY);
+        if (gyroListZ.Count > 5) {
+            gyroListZ.RemoveAt(0);
+        }
+
+        float countX = 0; 
+        foreach (float valueX in gyroListX) {
+
+        }
+
+        float countY = 0;
+        foreach (float valueY in gyroListY)
+        {
+            
+        }
+        float gY = countY / gyroListY.Count;
+
+        float countZ = 0;
+        foreach (float valueZ in gyroListZ)
+        {
+            countZ += valueZ;
+        }
+        float gZ = countX / gyroListZ.Count;
+
+        Quaternion rot = Quaternion.Euler(gX, gY, gZ);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 6.0f);
+    }
+}
---
+++ lsm9ds1_first_script/lsm9ds1_first_script.ino
@@ -0,0 +1,85 @@
+#include <Wire.h>
+#include <WiFi.h>
+#include <SPI.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h>  // not used in this demo but required!
+
+// i2c
+//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+
+#define LSM9DS1_SCK 18
+#define LSM9DS1_MISO 19
+#define LSM9DS1_MOSI 23
+#define LSM9DS1_XGCS 5
+#define LSM9DS1_MCS 15
+// You can also use software SPI
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_SCK, LSM9DS1_MISO, LSM9DS1_MOSI, LSM9DS1_XGCS, LSM9DS1_MCS);
+// Or hardware SPI! In this case, only CS pins are passed in
+//Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1(LSM9DS1_XGCS, LSM9DS1_MCS);
+
+ 
+
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
+  
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
+}
+
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  while (!Serial) {
+    delay(1); // will pause Zero, Leonardo, etc until serial console opens
+  }
+  
+  Serial.println("LSM9DS1 data read demo");
+  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+    while (1);
+  }
+  Serial.println("Found LSM9DS1 9DOF");
+
+  // helper to just set the default scaling we want, see above!
+  setupSensor(); 
+}
+
+void loop() 
+{
+  lsm.read();  /* ask it to read in the data */ 
+
+  /* Get a new sensor event */ 
+  sensors_event_t a, m, g, temp;
+
+  lsm.getEvent(&a, &m, &g, &temp); 
+// Prints accel in the format "x,y,z" m/s^2
+Serial.print(a.acceleration.x); Serial.print(","); Serial.print(a.acceleration.y); Serial.print(","); Serial.print(a.acceleration.z);
+Serial.println();
+// Prints gyro in format "x,y,z" dps
+Serial.print(g.gyro.x); Serial.print(","); Serial.print(g.gyro.y); Serial.print(","); Serial.print(g.gyro.z);
+Serial.println();
+// Prints mag in formation "x,y,z" gauss
+Serial.print(m.magnetic.x); Serial.print(","); Serial.print(m.magnetic.y); Serial.print(","); Serial.print(m.magnetic.z);
+Serial.println();
+
+  Serial.println();
+  delay(2000);
+}
